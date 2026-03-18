33a530804497a2ef7d0d8fe30141357ea3f3b3e8
Alexander Likhachev <65253792+avlihachev@users.noreply.github.com>
2026-03-18 16:57:15 +0200

    scaffold: wails project with lit frontend

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	app := NewApp()
+
+	err := wails.Run(&options.App{
+		Title:            "Claude Sessions Monitor",
+		Width:            320,
+		Height:           400,
+		Frameless:        true,
+		StartHidden:      true,
+		HideWindowOnClose: true,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		OnShutdown:       app.shutdown,
+		Bind: []interface{}{
+			app,
+		},
+		Mac: &mac.Options{
+			TitleBar: mac.TitleBarHidden(),
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
